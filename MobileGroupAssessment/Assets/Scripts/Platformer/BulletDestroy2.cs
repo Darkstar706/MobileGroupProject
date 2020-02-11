0b13d815b39000acf44781117c66f7a743d620f9
Alejandro Rodriguez <20.alejandro.rodrigu@ksd.org>
2020-02-11 12:56:11 -0800

    created unity project and test scenes as well as a basic pause menu and UI/player for a platformer

---
+++ MobileGroupAssessment/Assets/Scripts/Platformer/BulletDestroy2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDestroy2 : MonoBehaviour
+{
+    public GameObject deathParticle;
+    public float particleLifeTime = 1.0f;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+        Instantiate(deathParticle, transform.position, transform.rotation);
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+        Instantiate(deathParticle, transform.position, transform.rotation);
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}

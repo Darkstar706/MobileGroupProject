1ea200c2f38ae7fc5d9d3511f27c221113910077
Alejandro Rodriguez <20.alejandro.rodrigu@ksd.org>
2020-02-13 13:27:50 -0800

    added explosion force script and save script

--- MobileGroupAssessment/Assets/Scripts/Platformer/BulletDestroy2.cs
+++ MobileGroupAssessment/Assets/Scripts/Platformer/BulletDestroy2.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class BulletDestroy2 : MonoBehaviour
 {
-    public GameObject deathParticle;
     public float particleLifeTime = 1.0f;
     void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
-        Instantiate(deathParticle, transform.position, transform.rotation);
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
-        Instantiate(deathParticle, transform.position, transform.rotation);
-
         if (collision.gameObject.tag == "Enemy")
         {
             Destroy(gameObject);
---
+++ MobileGroupAssessment/Assets/Scripts/Platformer/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int level = 3;
+    public int health = 40;
+
+    public Text LevelValue;
+    public Text HealthValue;
+
+    private void Start()
+    {
+        LevelValue.text = "" + level;
+        HealthValue.text = "" + health;
+    }
+    public void SavePlayer()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+    public void Loadplayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        level = data.level;
+        health = data.health;
+        LevelValue.text = "" + level;
+        HealthValue.text = "" + health;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+    }
+    public void IncreaseLevel()
+    {
+        level++;
+        LevelValue.text = "" + level;
+    }
+    public void DecreaseLevel()
+    {
+        level--;
+        LevelValue.text = "" + level;
+    }
+    public void IncreaseHealth()
+    {
+        health++;
+        HealthValue.text = "" + health;
+    }
+    public void DecreaseHealth()
+    {
+        health--;
+        HealthValue.text = "" + health;
+    }
+}

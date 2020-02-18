b259b2acdde3d7b374a1f1dae8853a5a300f7f01
Alejandro Rodriguez <20.alejandro.rodrigu@ksd.org>
2020-02-18 11:54:03 -0800

    test

--- MobileGroupAssessment/Assets/Scripts/UI/TowerPlace.cs
+++ MobileGroupAssessment/Assets/Scripts/UI/TowerPlace.cs
@@ -19,5 +19,25 @@ public class TowerPlace : MonoBehaviour
             tower = (GameObject)
                 Instantiate(towerPrefab, transform.position, Quaternion.identity);
         }
+        else if (CanUpgradeMonster())
+        {
+            tower.GetComponent<TowerData>().IncreaseLevel();
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+            // TODO: Deduct gold
+        }
+    }
+    private bool CanUpgradeMonster()
+    {
+        if (tower != null)
+        {
+            TowerData monsterData = tower.GetComponent<TowerData>();
+            TowerLevel nextLevel = monsterData.GetNextLevel();
+            if (nextLevel != null)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }

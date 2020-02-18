680ae7e71821bf87c9e69505903fdaf4ec4c9968
Alejandro Rodriguez <20.alejandro.rodrigu@ksd.org>
2020-02-18 11:34:23 -0800

    added tower place script

---
+++ MobileGroupAssessment/Assets/Scripts/UI/TowerPlace.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlace : MonoBehaviour
+{
+    public GameObject towerPrefab;
+    private GameObject tower;
+
+    private bool CanPlaceTower()
+    {
+        return tower = null;
+    }
+
+    void OnMouseUp()
+    {
+        if (CanPlaceTower())
+        {
+            tower = (GameObject)
+                Instantiate(towerPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
